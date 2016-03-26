b2d836ddafb4c314b2e6aaa40305708781feef1a
fktkrt <fktkrt@gmail.com>
2016-03-26 18:05:09 +0100

    Box.java szerkesztese

---
+++ Box.java
@@ -0,0 +1,16 @@
+package szoftProj;
+
+public class Box{
+    protected boolean dead = false;
+    protected Field currentField;
+
+    public Box(Field currentfield) {}
+
+    public void kill() { dead = true; }
+
+    public boolean isDead() { return dead; }
+
+    public void action() {}
+    public void accept(Player launcher, Field target) {}
+    public void accept(Field launcher, Player target) {}
+}
