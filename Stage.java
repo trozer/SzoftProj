61b52ddd82774469da5fce94b91ea4cb48f73b27
szjozsi <szalontaijozsi@gmail.com>
2016-03-26 18:01:28 +0100

    Box, ZPM, Portal nélkül, Road,Scale,Gate -el tesztelhető

--- Stage.java
+++ Stage.java
@@ -8,13 +8,13 @@ import java.util.List;
 public class Stage
 {
     private int allZPM;
-/*    private List<Unit> units;
-*/
+    private List<Unit> units;
+
     public Stage (int allZPM)
     {
         this.allZPM = allZPM;
- /*       units =  new ArrayList<Unit>();
- */
+        units =  new ArrayList<Unit>();
+
     }
 
     public void init(char[][] table, Game game)
@@ -86,40 +86,45 @@ public class Stage
             }
             else if (readingState == 1) //ha már unit információt olvasunk be, akkor a sorok kezdőkaraterét viszgáljuk
             {
-                Field fieldOni1i2 = fields[(int)(table[i][1])][(int)(table[i][2])]; //olvashatóság: a sornak mindig az 1-es és 2-es karaktere a pozíció
 
                 switch (table[i][0])
                 {
-/*                    case 'P':           //Player információ
+                    case 'P':           //Player információ
                         if(table[i].length == 5) //a helyesen paraméterezett player sor 5 elemű (P, x, y, dir, box)
                         {
-                            Player player = new Player(allZPM,this.charToDirection(table[i][3]),fieldOni1i2,game);
+
+                            //olvashatóság: a sor 1-es és 2-es karakterében lévő pozíció = fieldOntablei1i2
+                            Field fieldOntablei1i2 = fields[Character.getNumericValue(table[i][1])][Character.getNumericValue(table[i][2])];
+
+                            Player player = new Player(allZPM,this.charToDirection(table[i][3]),fieldOntablei1i2,game);
                             Skeleton.addPlayer(player);
                             Skeleton.registerHashCode(player.hashCode(), "player");     //regisztráluk a fvkiíratáshoz
                             this.units.add(player);
 
                             //field - player kapcsolat
-                            fieldOni1i2.addUnit(player);
+                            fieldOntablei1i2.addUnit(player);
 
-                            if(table[i][2] == '+')      //ha van nála doboz, akkor létrehozzuk azt
+                           /* if(table[i][4] == '+')      //ha van nála doboz, akkor létrehozzuk azt
                             {
-                                Box box = new Box(fieldOni1i2);
+                                Box box = new Box(fieldOntablei1i2);
                                 Skeleton.registerHashCode(box.hashCode(), "box");
                                 player.grabBox(box);
-                            }
+                            }*/
                         }
-                        else
+/*                        else
                         {
                             System.out.println("Hiba: a player információ sora nem 5 karakter hosszú");
                         }
-                        break;
-                    case 'B':           //Bullet információ
+ */                       break;
+ /*                   case 'B':           //Bullet információ
                         if(table[i].length == 5)
                         {
                             Action moveAction = new Action(ActionType.Move,this.charToDirection(table[i][3]), this.charToColor(table[i][4]));
                             Skeleton.registerHashCode(moveAction.hashCode(), "action");
 
-                            Bullet bullet = new Bullet(moveAction, fieldOni1i2);
+                            Field fieldOntablei1i2 = fields[Character.getNumericValue(table[i][1])][Character.getNumericValue(table[i][2])];
+                            //lövedék kapcsolatai: elég, ha csak a lövedék tud a mezőről, amin van a mezőnek (amin épp a player is áll) nem kell tudni (úgyis egyből elmegy róla)
+                            Bullet bullet = new Bullet(moveAction, fieldOntablei1i2);
                             Skeleton.registerHashCode(bullet.hashCode(), "bullet");
 
                             this.units.add(bullet);
@@ -132,10 +137,10 @@ public class Stage
                     case 'b':           //box információ
                         if(table[i].length == 3)
                         {
-                            Box box = new Box(fieldOni1i2);
+                            Field fieldOntablei1i2 = fields[Character.getNumericValue(table[i][1])][Character.getNumericValue(table[i][2])];
+                            Box box = new Box(fieldOntablei1i2);
                             Skeleton.registerHashCode(box.hashCode(), "box");
-                            fieldOni1i2.addUnit(box);
-
+                            fieldOntablei1i2.addUnit(box);
                         }
                         else
                         {
@@ -145,9 +150,9 @@ public class Stage
                     case 'z':           //ZPM információ
                         if(table[i].length == 3)
                         {
-                            ZPM zpm = new ZPM(fieldOni1i2);
+                            ZPM zpm = new ZPM(fieldOntablei1i2);
                             Skeleton.registerHashCode(zpm.hashCode(), "zpm");
-                            fieldOni1i2.addUnit(zpm);
+                            fieldOntablei1i2.addUnit(zpm);
                         }
                         else
                         {
@@ -157,26 +162,33 @@ public class Stage
                     case 'p':
                         if(table[i].length == 3)    //csak 1 kijárat nélküli portál van
                         {
-                            portal.createPortal(fieldOni1i2, Color.blue);
+                            Field fieldOntablei1i2 = fields[Character.getNumericValue(table[i][1])][Character.getNumericValue(table[i][2])];
+                            portal.createPortal(fieldOntablei1i2, Color.blue);
                         }
                         else if(table[i].length == 5)
                         {
-                            portal.createPortal(fieldOni1i2, Color.blue);
-                            portal.createPortal(fields[(int)(table[i][3])][(int)(table[i][4])], Color.blue);
+                            Field fieldOntablei1i2 = fields[Character.getNumericValue(table[i][1])][Character.getNumericValue(table[i][2])];
+                            Field fieldOntablei3i4 = fields[Character.getNumericValue(table[i][3])][Character.getNumericValue(table[i][4])];
+
+                            portal.createPortal(fieldOntablei1i2, Color.blue);
+                            portal.createPortal(fieldOntablei3i4, Color.yellow);
                         }
                         else
                         {
                             System.out.println("Hiba: a portál kapcsolat információ sora nem 3 vagy 5 karakter hosszú");
                         }
  */                   case 'c':
-                        if(table[i].length == 5)    //connection, gate sor, gate oszlop, scale sor, scale oszlop
-                        {
-                            ((Scale)(fields[(int)table[i][3]][(int)table[i][4]])).setGate((Gate)fieldOni1i2);
-                        }
-                        else
-                        {
-                            System.out.println("Hiba: a gate-scale kapcsolat információ sora nem 5 karakter hosszú");
-                        }
+                    if(table[i].length == 5)    //connection, gate sor, gate oszlop, scale sor, scale oszlop
+                    {
+                        Field fieldOntablei1i2 = fields[Character.getNumericValue(table[i][1])][Character.getNumericValue(table[i][2])];
+                        Field fieldOntablei3i4 = fields[Character.getNumericValue(table[i][3])][Character.getNumericValue(table[i][4])];
+
+                        ((Scale)fieldOntablei3i4).setGate((Gate)fieldOntablei1i2);
+                    }
+                    else
+                    {
+                        System.out.println("Hiba: a gate-scale kapcsolat információ sora nem 5 karakter hosszú");
+                    }
 
                     default:
                         System.out.println("Hiba: valamelyik Unit információ sora nem megfelelően kezdődik");
@@ -247,25 +259,25 @@ public class Stage
     //lefuttat egy kört
     public void update()
     {
-/*        if(units != null)
+        if(units != null)
         {
             for (Unit unit : units)
                 unit.action();
         }
-*/    }
+    }
 
-    //
+    //"halott" egységek törlése units listából
     public void collectUnits()
     {
-/*        for(Iterator<Unit> it = units.iterator(); it.hasNext();)
+        for(Iterator<Unit> it = units.iterator(); it.hasNext();)
         {
-            if(it.next().isDead)
+            if(it.next().isDead())
                 it.remove();
         }
-*/    }
+    }
 
     public void addUnit(Unit unit)
     {
-/*        this.units.add(unit);   //hozzáad 1 unitot, feltételezzük, hogy a létrehozás helyén be lett regisztrálva a Skeletonba
-*/    }
+        this.units.add(unit);   //hozzáad 1 unitot, feltételezzük, hogy a létrehozás helyén be lett regisztrálva a Skeletonba
+    }
 }
