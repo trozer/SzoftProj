4a02b61493dd2904637b608c377180d75a1ee064
fktkrt <fktkrt@gmail.com>
2016-03-27 20:55:45 +0200

    ZPM.java szerkesztese

--- ZPM.java
+++ ZPM.java
@@ -6,7 +6,23 @@ public class ZPM extends Unit{
 
     public ZPM(Field currentfield) {}
 
-    public void action() {}
-    public void accept(Player launcher, Field target) {}
-    public void accept(Field launcher, Player target) {}
+    public void accept(Field launcher, Player target) {
+        ArrayList<Object> parameters = new ArrayList<Object>();
+        parameters.add(bullet);
+        switch (target.getAction().getType()) {
+            case GRAB:
+                target.grabBox(this);
+                if (GRAB == true){
+                    launcher.removeUnit(this);
+                }
+                break;
+            default:
+                break;
+        }
+        parameters.add(Skeleton.getEmpty());
+        Skeleton.callMethod("accept", this, parameters);
+        Skeleton.returnMethod("accept", this, parameters);
+
+
+    }
 }
