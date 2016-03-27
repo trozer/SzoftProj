fd91334006f27b09978c707398fd3117bf9bf0c1
fktkrt <fktkrt@gmail.com>
2016-03-27 20:52:35 +0200

    Box.java szerkesztese

--- Box.java
+++ Box.java
@@ -7,7 +7,39 @@ public class Box extends Unit{
     public Box(Field currentfield) {}
 
     public void accept(Player launcher, Field target) {
+        ArrayList<Object> parameters = new ArrayList<Object>();
+        parameters.add(bullet);
+        switch (target.getAction().getType()) {
+            case DROP:
+                target.addUnit(this);
+                if (DROP == true){
+                    launcher.dropBox();
+                }
+                break;
+            default:
+                break;
+        }
+        parameters.add(Skeleton.getEmpty());
+        Skeleton.callMethod("accept", this, parameters);
+        Skeleton.returnMethod("accept", this, parameters);
+    }
+    public void accept(Field launcher, Player target) {
+        ArrayList<Object> parameters = new ArrayList<Object>();
+        parameters.add(bullet);
+        switch (target.getAction().getType()) {
+            case GRAB:
+                target.grabBox(this);
+                if (GRAB == true){
+                    launcher.removeUnit(this);
+                }
+                break;
+            default:
+                break;
+        }
+        parameters.add(Skeleton.getEmpty());
+        Skeleton.callMethod("accept", this, parameters);
+        Skeleton.returnMethod("accept", this, parameters);
+
 
     }
-    public void accept(Field launcher, Player target) {}
 }
