b5bff4fda2e0ac41374641a6e3b09b6d8a22c74c
Kiss Attila <Kiss Attila>
2016-04-23 23:34:17 +0200

    nextAction mindig valamilyen iranyba mozgas

--- Replicator.java
+++ Replicator.java
@@ -1,5 +1,7 @@
 package szoftProj;
 
+import java.util.Random;
+
 public class Replicator extends ActionUnit{
 	
 	private Game game;
@@ -12,6 +14,22 @@ public class Replicator extends ActionUnit{
 		this.nextAction = new Action(ActionType.MOVE, direction, null);
 	}
 
+	//A replicator uj action-jet allitja elo.
+	private Action makeNextAction() {
+		Direction dir;
+		Random r = new Random();
+		int i = r.nextInt(7); //0-6
+		
+		if (i < 3) dir = currentDirection;
+		else if (i == 3) dir = Direction.NORTH;
+		else if (i == 4) dir = Direction.EAST;
+		else if (i == 5) dir = Direction.WEST;
+		else dir = Direction.SOUTH;
+		
+		Action nextA = new Action(ActionType.MOVE, dir, null);
+		return nextA;
+	}
+	
 	//Kezdemenyezi a mezo cserejet (utra), majd megoli magat
 	public void replaceField(){
 		game.replaceField(currentField);
@@ -19,11 +37,13 @@ public class Replicator extends ActionUnit{
 		kill();
 	}
 	
-	//Eltavolitja magat a jelenlegi mezorol, majd hozzaadja magat a parameterkent kapott uj mezohoz,
-	//es beallitja a currentFieldet az uj mezore. Illetve választ 
+	//Eltavolitja magat a jelenlegi mezorol,
+	//es beallitja a currentFieldet az uj mezore. Illetve beallit maganak egy uj mozgast.
 	public void step(Field target){
 		currentField.removeUnit();
 		currentField = target;
+		
+		nextAction = makeNextAction();
 	}
 	
 	//A nextAction alapjan csinal valamit.
@@ -31,12 +51,6 @@ public class Replicator extends ActionUnit{
 		//Ha move, akkor a megfelelo iranyban levo szomszedos mezo doo-jat hivja meg.
 		if (nextAction.getType() == ActionType.MOVE){
 			currentField.getNeighbourInDirection(currentDirection).doo(this);
-			nextAction = null;
-		}
-		
-		//Ha turn, akkor beallitja az uj iranyt.
-		else if (nextAction.getType() == ActionType.TURN){
-			currentDirection = nextAction.getDirection();
 			nextAction = null;
 		}
 	}
